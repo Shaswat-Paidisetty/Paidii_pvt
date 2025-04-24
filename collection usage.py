e3f3a120afeb4f49bd6f7bc7c7efdb5c527ba68d
Shaswat-Paidisetty <136492714+Shaswat-Paidisetty@users.noreply.github.com>
2025-04-24 10:52:42 +0530

    Update collection usage.py

--- collection usage.py
+++ collection usage.py
@@ -1,9 +1,8 @@
 from pymongo import MongoClient
 
 # MongoDB connection setup
-client = MongoClient("mongodb+srv://prod_read_access_user:LQFtkbsod9d9wuyk@elabsproduction.lagpo.mongodb.net/?retryWrites=true&w=majority&appName=ElabsProduction")
-db = client["itclabs-prod"]
-collection = db["el_parameter_category"]
+client = MongoClient(""mongodb+srv://Paidii:Paidisetty%4010@cluster0.kfocv.mongodb.net/""]
+collection = db["Dummy"]
 
 # Fetch all documents from the collection
 total_documents = collection.count_documents({})
@@ -18,7 +17,7 @@ for doc in collection.find():
             field_usage[field] = field_usage.get(field, 0) + 1
 
 # Calculate and display usage percentage for each field
-print("Field usage in collection 'el_category':")
+print("Field usage in collection 'Dummy':")
 for field, count in field_usage.items():
     usage_percentage = (count / total_documents) * 100 if total_documents > 0 else 0
     print(f"{field}: {usage_percentage:.2f}%")
